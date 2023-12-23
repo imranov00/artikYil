d76e7428e1d3e33f50219b240ca44be9494a08fc
İmranov <semtexpert@gmail.com>
2023-12-23 16:42:22 +0300

    Artık Yıl Versiyon v1.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Yıl Giriniz: ");
+        int yil = scanner.nextInt();
+
+
+        boolean artikYil = false;
+
+
+        if (yil % 4 == 0) {
+            if (yil % 100 != 0 || yil % 400 == 0) {
+                artikYil = true;
+            }
+        }
+
+
+        if (artikYil) {
+            System.out.println(yil + " bir artık yıldır.");
+        } else {
+            System.out.println(yil + " bir artık yıl değildir.");
+        }
     }
 }
